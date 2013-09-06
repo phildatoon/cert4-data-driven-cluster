f0cd9af16c6567991adba2ba0590d660dc9d18f0
Phil Datoon <phil.datoon@yahoo.com>
2013-09-06 23:52:55 +0800

    Using LINQ queries

---
+++ LINQ Queries/LINQ Queries/LINQ Queries.cs
@@ -0,0 +1,49 @@
+﻿// Datoon, Philip Bryan B.
+// 131311399
+// 2 September 2013
+// Using LINQ queries (pp 262-266)
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LINQ_Queries
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] countries = new string[17] {"Antarctica",
+                "Akrotiri", "Algeria", "Albania", "Australia", "Andorra",
+                "Angola", "Anguilla", "Antigua and Barbuda",
+                "Ashmore and Cartier Islands", "Argentina", "Azerbaijan",
+                "Armenia", "American Samoa", "Afghanistan", "Austria", "Aruba"};
+
+            // sets the query
+            var query = from country in countries       // gets each array element
+                        where country.EndsWith("ia")    // sets limitation for display
+            //          where country.StartsWith("An")
+            //          wherecountry.Length >= 7 && country.Length <= 10
+                        orderby country                 // sorts array alphabetically
+            //          orderby country.Length, country // sorts array by character length
+                        select country;                 // selects from the array
+
+            // displays array in list box
+            foreach (var value in query)
+            {
+                listBox1.Items.Add(value);
+            }
+        }
+    }
+}
